ebf559c6abcfb239ebea3b1fcd16959e536eea04
Pedro Sánchez Terraf <pedrost@gmail.com>
2024-09-18 18:31:40 -0300

    Simplified version of the Tuxedo wallet

---
+++ wallet/src/main.rs
@@ -0,0 +1,228 @@
+//! A simple CLI wallet. For now it is a toy just to start testing things out.
+
+use clap::Parser;
+use jsonrpsee::http_client::HttpClientBuilder;
+use parity_scale_codec::{Decode, Encode};
+use sp_core::H256;
+use std::path::PathBuf;
+use griffin_core::types::OutputRef;
+
+mod cli;
+mod keystore;
+mod money;
+mod rpc;
+mod sync;
+
+use cli::{Cli, Command};
+
+/// The default RPC endpoint for the wallet to connect to
+const DEFAULT_ENDPOINT: &str = "http://localhost:9944";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    // Parse command line args
+    let cli = Cli::parse();
+
+    // If the user specified --tmp or --dev, then use a temporary directory.
+    let tmp = cli.tmp || cli.dev;
+
+    // Setup the data paths.
+    let data_path = match tmp {
+        true => temp_dir(),
+        _ => cli.base_path.unwrap_or_else(default_data_path),
+    };
+    let keystore_path = data_path.join("keystore");
+    let db_path = data_path.join("wallet_database");
+
+    // Setup the keystore
+    let keystore = sc_keystore::LocalKeystore::open(keystore_path.clone(), None)?;
+
+    if cli.dev {
+        // Insert the example Shawn key so example transactions can be signed.
+        crate::keystore::insert_development_key_for_this_session(&keystore)?;
+    }
+
+    // Setup jsonrpsee and endpoint-related information.
+    // https://github.com/paritytech/jsonrpsee/blob/master/examples/examples/http.rs
+    let client = HttpClientBuilder::default().build(cli.endpoint)?;
+
+    // Read node's genesis block.
+    let node_genesis_hash = rpc::node_get_block_hash(0, &client)
+        .await?
+        .expect("node should be able to return some genesis hash");
+    let node_genesis_block = rpc::node_get_block(node_genesis_hash, &client)
+        .await?
+        .expect("node should be able to return some genesis block");
+    log::debug!("Node's Genesis block::{:?}", node_genesis_hash);
+
+    // Open the local database
+    let db = sync::open_db(db_path, node_genesis_hash, node_genesis_block.clone())?;
+
+    let num_blocks =
+        sync::height(&db)?.expect("db should be initialized automatically when opening.");
+    log::info!("Number of blocks in the db: {num_blocks}");
+
+    if !sled::Db::was_recovered(&db) {
+        sync::apply_block(&db, node_genesis_block, node_genesis_hash, &true).await?;
+    }
+
+    // Synchronize the wallet with attached node unless instructed otherwise.
+    if cli.no_sync {
+        log::warn!("Skipping sync with node. Using previously synced information.")
+    } else {
+        sync::synchronize(&db, &client, &true).await?;
+
+        log::info!(
+            "Wallet database synchronized with node to height {:?}",
+            sync::height(&db)?.expect("We just synced, so there is a height available")
+        );
+    }
+
+    // Dispatch to proper subcommand
+    match cli.command {
+        Some(Command::MintCoins(args)) => {
+            money::mint_coins(&client, args).await
+        }
+        Some(Command::VerifyCoin { output_ref }) => {
+            println!("Details of coin {}:", hex::encode(output_ref.encode()));
+
+            // Print the details from storage
+            let coin_from_storage = money::get_coin_from_storage(&output_ref, &client).await?;
+            print!("Found in storage.  Value: {:?}, ", coin_from_storage);
+
+            // Print the details from the local db
+            match sync::get_unspent(&db, &output_ref)? {
+                Some((owner, amount)) => {
+                    println!("Found in local db. Value: {amount}, owned by {owner}");
+                }
+                None => {
+                    println!("Not found in local db");
+                }
+            }
+
+            Ok(())
+        }
+        Some(Command::SpendCoins(args)) => {
+            money::spend_coins(&db, &client, &keystore, args).await
+        }
+        Some(Command::InsertKey { seed }) => crate::keystore::insert_key(&keystore, &seed),
+        Some(Command::GenerateKey { password }) => {
+            crate::keystore::generate_key(&keystore, password)?;
+            Ok(())
+        }
+        Some(Command::ShowKeys) => {
+            crate::keystore::get_keys(&keystore)?.for_each(|pubkey| {
+                println!("key: 0x{}", hex::encode(pubkey));
+            });
+
+            Ok(())
+        }
+        Some(Command::RemoveKey { pub_key }) => {
+            println!("CAUTION!!! About permanently remove {pub_key}. This action CANNOT BE REVERSED. Type \"proceed\" to confirm deletion.");
+
+            let mut confirmation = String::new();
+            std::io::stdin()
+                .read_line(&mut confirmation)
+                .expect("Failed to read line");
+
+            if confirmation.trim() == "proceed" {
+                crate::keystore::remove_key(&keystore_path, &pub_key)
+            } else {
+                println!("Deletion aborted. That was close.");
+                Ok(())
+            }
+        }
+        Some(Command::ShowBalance) => {
+            println!("Balance Summary");
+            let mut total = 0;
+            let balances = sync::get_balances(&db)?;
+            for (account, balance) in balances {
+                total += balance;
+                println!("{account}: {balance}");
+            }
+            println!("--------------------");
+            println!("total      : {total}");
+
+            Ok(())
+        }
+        Some(Command::ShowAllOutputs) => {
+            println!("###### Unspent outputs ###########");
+            sync::print_unspent_tree(&db)?;
+
+            Ok(())
+        }
+        None => {
+            log::info!("No Wallet Command invoked. Exiting.");
+            Ok(())
+        }
+    }?;
+
+    if tmp {
+        // Cleanup the temporary directory.
+        std::fs::remove_dir_all(data_path.clone()).map_err(|e| {
+            log::warn!(
+                "Unable to remove temporary data directory at {}\nPlease remove it manually.",
+                data_path.to_string_lossy()
+            );
+            e
+        })?;
+    }
+
+    Ok(())
+}
+
+/// Parse a string into an H256 that represents a public key
+pub(crate) fn h256_from_string(s: &str) -> anyhow::Result<H256> {
+    let s = strip_0x_prefix(s);
+
+    let mut bytes: [u8; 32] = [0; 32];
+    hex::decode_to_slice(s, &mut bytes as &mut [u8])
+        .map_err(|_| clap::Error::new(clap::error::ErrorKind::ValueValidation))?;
+    Ok(H256::from(bytes))
+}
+
+/// Parse an output ref from a string
+fn output_ref_from_string(s: &str) -> Result<OutputRef, clap::Error> {
+    let s = strip_0x_prefix(s);
+    let bytes =
+        hex::decode(s).map_err(|_| clap::Error::new(clap::error::ErrorKind::ValueValidation))?;
+
+    OutputRef::decode(&mut &bytes[..])
+        .map_err(|_| clap::Error::new(clap::error::ErrorKind::ValueValidation))
+}
+
+/// Takes a string and checks for a 0x prefix. Returns a string without a 0x prefix.
+fn strip_0x_prefix(s: &str) -> &str {
+    if &s[..2] == "0x" {
+        &s[2..]
+    } else {
+        s
+    }
+}
+
+/// Generate a plaform-specific temporary directory for the wallet
+fn temp_dir() -> PathBuf {
+    // Since it is only used for testing purpose, we don't need a secure temp dir, just a unique one.
+    std::env::temp_dir().join(format!(
+        "griffin-wallet-{}",
+        std::time::UNIX_EPOCH.elapsed().unwrap().as_millis(),
+    ))
+}
+
+/// Generate the platform-specific default data path for the wallet
+fn default_data_path() -> PathBuf {
+    // This uses the directories crate.
+    // https://docs.rs/directories/latest/directories/struct.ProjectDirs.html
+
+    // Application developers may want to put actual qualifiers or organization here
+    let qualifier = "";
+    let organization = "";
+    let application = env!("CARGO_PKG_NAME");
+
+    directories::ProjectDirs::from(qualifier, organization, application)
+        .expect("app directories exist on all supported platforms; qed")
+        .data_dir()
+        .into()
+}
---
+++ wallet/src/money.rs
@@ -0,0 +1,179 @@
+//! Wallet features related to spending money and checking balances.
+
+use crate::{cli::MintCoinArgs, cli::SpendArgs, rpc::fetch_storage, sync};
+
+use anyhow::anyhow;
+use jsonrpsee::{core::client::ClientT, http_client::HttpClient, rpc_params};
+use parity_scale_codec::Encode;
+use sc_keystore::LocalKeystore;
+use sled::Db;
+use sp_core::H256;
+use sp_runtime::traits::{BlakeTwo256, Hash};
+use griffin_core::{
+    types::{Coin, Input, Output, OutputRef, Transaction},
+};
+
+/// Create and send a transaction that mints the coins on the network
+pub async fn mint_coins(
+    client: &HttpClient,
+    args: MintCoinArgs,
+) -> anyhow::Result<()> {
+    mint_coins_helper(client, args).await
+}
+
+pub async fn mint_coins_helper(client: &HttpClient, args: MintCoinArgs) -> anyhow::Result<()> {
+    log::debug!("The args are:: {:?}", args);
+
+    let transaction: griffin_core::types::Transaction = Transaction {
+        inputs: Vec::new(),
+        outputs: vec![Output {
+            payload: args.amount,
+            owner: H256::from_slice(b"                                "),
+        }],
+    };
+
+    let encoded_tx = hex::encode(transaction.encode());
+    let params = rpc_params![encoded_tx];
+    let spawn_response: Result<String, _> = client.request("author_submitExtrinsic", params).await;
+
+    log::info!(
+        "Node's response to mint-coin transaction: {:?}",
+        spawn_response
+    );
+
+    let minted_coin_ref = OutputRef {
+        tx_hash: <BlakeTwo256 as Hash>::hash_of(&transaction.encode()),
+        index: 0,
+    };
+    let output = &transaction.outputs[0];
+    let amount = output.payload;
+    println!(
+        "Minted {:?} worth {amount}. ",
+        hex::encode(minted_coin_ref.encode())
+    );
+
+    Ok(())
+}
+
+/// Create and send a transaction that spends coins on the network
+pub async fn spend_coins(
+    db: &Db,
+    client: &HttpClient,
+    keystore: &LocalKeystore,
+    args: SpendArgs,
+) -> anyhow::Result<()> {
+    spend_coins_helper(db, client, keystore, args).await
+}
+
+pub async fn spend_coins_helper(
+    db: &Db,
+    client: &HttpClient,
+    _keystore: &LocalKeystore,
+    args: SpendArgs,
+) -> anyhow::Result<()> {
+    log::debug!("The args are:: {:?}", args);
+
+    // Construct a template Transaction to push coins into later
+    let mut transaction: Transaction = Transaction {
+        inputs: Vec::new(),
+        outputs: Vec::new(),
+    };
+
+    // Construct each output and then push to the transactions
+    let mut total_output_amount: u64 = 0;
+    for amount in &args.output_amount {
+        let output = Output {
+            payload: *amount,
+            owner: H256::from_slice(b"                                "),
+        };
+        total_output_amount += *amount;
+        transaction.outputs.push(output);
+    }
+
+    // The total input set will consist of any manually chosen inputs
+    // plus any automatically chosen to make the input amount high enough
+    let mut total_input_amount: u64 = 0;
+    let mut all_input_refs = args.input;
+    for output_ref in &all_input_refs {
+        let (_owner_pubkey, amount) = sync::get_unspent(db, output_ref)?.ok_or(anyhow!(
+            "user-specified output ref not found in local database"
+        ))?;
+        total_input_amount += amount;
+    }
+
+    // If the supplied inputs are not valuable enough to cover the output amount
+    // we select the rest arbitrarily from the local db. (In many cases, this will be all the inputs.)
+    if total_input_amount < total_output_amount {
+        match sync::get_arbitrary_unspent_set(db, total_output_amount - total_input_amount)? {
+            Some(more_inputs) => {
+                all_input_refs.extend(more_inputs);
+            }
+            None => Err(anyhow!(
+                "Not enough value in database to construct transaction"
+            ))?,
+        }
+    }
+
+    // Make sure each input decodes and is still present in the node's storage,
+    // and then push to transaction.
+    for output_ref in &all_input_refs {
+        get_coin_from_storage(output_ref, client).await?;
+        transaction.inputs.push(Input {
+            output_ref: output_ref.clone(),
+        });
+    }
+
+    log::debug!("signed transactions is: {:#?}", transaction);
+
+    // Send the transaction
+    let genesis_spend_hex = hex::encode(transaction.encode());
+    let params = rpc_params![genesis_spend_hex];
+    let genesis_spend_response: Result<String, _> =
+        client.request("author_submitExtrinsic", params).await;
+    log::info!(
+        "Node's response to spend transaction: {:?}",
+        genesis_spend_response
+    );
+
+    // Print new output refs for user to check later
+    let tx_hash = <BlakeTwo256 as Hash>::hash_of(&transaction.encode());
+    for (i, output) in transaction.outputs.iter().enumerate() {
+        let new_coin_ref = OutputRef {
+            tx_hash,
+            index: i as u32,
+        };
+        let amount = output.payload;
+
+        println!(
+            "Created {:?} worth {amount}. ",
+            hex::encode(new_coin_ref.encode())
+        );
+    }
+
+    Ok(())
+}
+
+/// Given an output ref, fetch the details about this coin from the node's
+/// storage.
+pub async fn get_coin_from_storage(
+    output_ref: &OutputRef,
+    client: &HttpClient,
+) -> anyhow::Result<Coin> {
+    let utxo = fetch_storage(output_ref, client).await?;
+    let coin_in_storage: Coin = utxo.payload;
+
+    Ok(coin_in_storage)
+}
+
+/// Apply a transaction to the local database, storing the new coins.
+pub(crate) fn apply_transaction(
+    db: &Db,
+    tx_hash: <BlakeTwo256 as Hash>::Output,
+    index: u32,
+    output: &Output,
+) -> anyhow::Result<()> {
+    let amount = output.payload;
+    let output_ref = OutputRef { tx_hash, index };
+    let owner_pubkey = H256::from_slice(b"                                ");
+    crate::sync::add_unspent_output(db, &output_ref, &owner_pubkey, &amount)
+}
