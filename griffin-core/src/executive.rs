04c04c3a3aaf53b5e338b2d3bb2d198eaf953cd4
Franco Luque <francolq@gmail.com>
2024-10-04 13:31:03 -0300

    Add pallas-applying and call validate over a test tx form griffin-core executive.

--- griffin-core/src/executive.rs
+++ griffin-core/src/executive.rs
@@ -25,10 +25,141 @@ use sp_runtime::{
     ApplyExtrinsicResult, ExtrinsicInclusionMode, StateVersion,
 };
 use alloc::{collections::btree_set::BTreeSet, vec::Vec};
+use alloc::string::String;
+use alloc::borrow::Cow;
+use alloc::boxed::Box;
+use core::iter::zip;
+use alloc::vec;
+
+use pallas_applying::{
+    validate, UTxOs,
+    utils::{
+        Environment, MultiEraProtocolParameters, BabbageProtParams
+    },
+};
+use pallas_primitives::babbage::{
+    MintedTx, Value, MintedDatumOption, MintedScriptRef,
+    PseudoTransactionOutput, MintedPostAlonzoTransactionOutput,
+    MintedTransactionOutput, MintedTransactionBody,
+    ExUnits, ExUnitPrices, CostMdls, RationalNumber,
+    Nonce, NonceVariant,
+};
+use pallas_traverse::{MultiEraTx, MultiEraInput, MultiEraOutput};
+use pallas_codec::utils::{CborWrap, Bytes};
 
 /// The executive. Each runtime is encouraged to make a type alias called `Executive` that fills
 /// in the proper generic types.
 pub struct Executive;
+
+pub fn babbage_minted_tx_from_cbor(tx_cbor: &[u8]) -> MintedTx<'_> {
+    pallas_codec::minicbor::decode::<MintedTx>(&tx_cbor[..]).unwrap()
+}
+
+pub fn mk_utxo_for_babbage_tx<'a>(
+    tx_body: &MintedTransactionBody,
+    tx_outs_info: &'a [(
+        String, // address in string format
+        Value,
+        Option<MintedDatumOption>,
+        Option<CborWrap<MintedScriptRef>>,
+    )],
+) -> UTxOs<'a> {
+    let mut utxos: UTxOs = UTxOs::new();
+    for (tx_in, (addr, val, datum_opt, script_ref)) in zip(tx_body.inputs.clone(), tx_outs_info) {
+        let multi_era_in: MultiEraInput =
+            MultiEraInput::AlonzoCompatible(Box::new(Cow::Owned(tx_in)));
+        let address_bytes: Bytes = match hex::decode(addr) {
+            Ok(bytes_vec) => Bytes::from(bytes_vec),
+            _ => panic!("Unable to decode input address"),
+        };
+        let tx_out: MintedTransactionOutput =
+            PseudoTransactionOutput::PostAlonzo(MintedPostAlonzoTransactionOutput {
+                address: address_bytes,
+                value: val.clone(),
+                datum_option: datum_opt.clone(),
+                script_ref: script_ref.clone(),
+            });
+        let multi_era_out: MultiEraOutput = MultiEraOutput::Babbage(Box::new(Cow::Owned(tx_out)));
+        utxos.insert(multi_era_in, multi_era_out);
+    }
+    utxos
+}
+
+fn mk_mainnet_params_epoch_365() -> BabbageProtParams {
+    BabbageProtParams {
+        minfee_a: 44,
+        minfee_b: 155381,
+        max_block_body_size: 90112,
+        max_transaction_size: 16384,
+        max_block_header_size: 1100,
+        key_deposit: 2000000,
+        pool_deposit: 500000000,
+        maximum_epoch: 18,
+        desired_number_of_stake_pools: 500,
+        pool_pledge_influence: RationalNumber {
+            numerator: 3,
+            denominator: 10,
+        },
+        expansion_rate: RationalNumber {
+            numerator: 3,
+            denominator: 1000,
+        },
+        treasury_growth_rate: RationalNumber {
+            numerator: 2,
+            denominator: 10,
+        },
+        decentralization_constant: RationalNumber {
+            numerator: 0,
+            denominator: 1,
+        },
+        extra_entropy: Nonce {
+            variant: NonceVariant::NeutralNonce,
+            hash: None,
+        },
+        protocol_version: (7, 0),
+        min_pool_cost: 340000000,
+        ada_per_utxo_byte: 4310,
+        cost_models_for_script_languages: CostMdls {
+            plutus_v1: Some(vec![
+                197209, 0, 1, 1, 396231, 621, 0, 1, 150000, 1000, 0, 1, 150000, 32, 2477736,
+                29175, 4, 29773, 100, 29773, 100, 29773, 100, 29773, 100, 29773, 100, 29773,
+                100, 100, 100, 29773, 100, 150000, 32, 150000, 32, 150000, 32, 150000, 1000, 0,
+                1, 150000, 32, 150000, 1000, 0, 8, 148000, 425507, 118, 0, 1, 1, 150000, 1000,
+                0, 8, 150000, 112536, 247, 1, 150000, 10000, 1, 136542, 1326, 1, 1000, 150000,
+                1000, 1, 150000, 32, 150000, 32, 150000, 32, 1, 1, 150000, 1, 150000, 4,
+                103599, 248, 1, 103599, 248, 1, 145276, 1366, 1, 179690, 497, 1, 150000, 32,
+                150000, 32, 150000, 32, 150000, 32, 150000, 32, 150000, 32, 148000, 425507,
+                118, 0, 1, 1, 61516, 11218, 0, 1, 150000, 32, 148000, 425507, 118, 0, 1, 1,
+                148000, 425507, 118, 0, 1, 1, 2477736, 29175, 4, 0, 82363, 4, 150000, 5000, 0,
+                1, 150000, 32, 197209, 0, 1, 1, 150000, 32, 150000, 32, 150000, 32, 150000, 32,
+                150000, 32, 150000, 32, 150000, 32, 3345831, 1, 1,
+            ]),
+
+            plutus_v2: None,
+        },
+        execution_costs: ExUnitPrices {
+            mem_price: RationalNumber {
+                numerator: 577,
+                denominator: 10000,
+            },
+            step_price: RationalNumber {
+                numerator: 721,
+                denominator: 10000000,
+            },
+        },
+        max_tx_ex_units: ExUnits {
+            mem: 14000000,
+            steps: 10000000000,
+        },
+        max_block_ex_units: ExUnits {
+            mem: 62000000,
+            steps: 40000000000,
+        },
+        max_value_size: 5000,
+        collateral_percentage: 150,
+        max_collateral_inputs: 3,
+    }
+}
 
 impl Executive
 where
@@ -46,6 +177,32 @@ where
             target: LOG_TARGET,
             "validating griffin transaction",
         );
+
+        let cbor_bytes: Vec<u8> = hex::decode(include_str!("../babbage3.tx")).unwrap();
+        let mtx: MintedTx = babbage_minted_tx_from_cbor(&cbor_bytes);
+        let metx: MultiEraTx = MultiEraTx::from_babbage(&mtx);
+        let tx_outs_info: &[(
+            String,
+            Value,
+            Option<MintedDatumOption>,
+            Option<CborWrap<MintedScriptRef>>,
+        )] = &[(
+            String::from("011be1f490912af2fc39f8e3637a2bade2ecbebefe63e8bfef10989cd6f593309a155b0ebb45ff830747e61f98e5b77feaf7529ce9df351382"),
+            Value::Coin(103324335),
+            None,
+            None,
+        )];
+        let utxos: UTxOs = mk_utxo_for_babbage_tx(&mtx.transaction_body, tx_outs_info);
+        let env: Environment = Environment {
+            prot_params: MultiEraProtocolParameters::Babbage(mk_mainnet_params_epoch_365()),
+            prot_magic: 764824073,
+            block_slot: 72316896,
+            network_id: 1,
+        };
+        match validate(&metx, &utxos, &env) {
+            Ok(()) => (),
+            Err(err) => assert!(false, "Unexpected error ({:?})", err),
+        }
 
         // There must be at least one input
         ensure!(!transaction.transaction_body.inputs.is_empty(), UtxoError::NoInputs);
