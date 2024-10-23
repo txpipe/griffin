7a1d8b884ef606274e533cbf81bf7393565ff891
Francisco Joray <franjoray@gmail.com>
2024-10-22 21:55:03 -0300

    added `uplc` directory

---
+++ uplc/src/optimize.rs
@@ -0,0 +1,28 @@
+use crate::ast::{Name, Program};
+
+pub mod interner;
+pub mod shrinker;
+
+pub fn aiken_optimize_and_intern(program: Program<Name>) -> Program<Name> {
+    program
+        .inline_constr_ops()
+        .bls381_compressor()
+        .builtin_force_reducer()
+        .lambda_reducer()
+        .inline_reducer()
+        .identity_reducer()
+        .lambda_reducer()
+        .inline_reducer()
+        .force_delay_reducer()
+        .cast_data_reducer()
+        .builtin_eval_reducer()
+        .convert_arithmetic_ops()
+        .builtin_curry_reducer()
+        .lambda_reducer()
+        .inline_reducer()
+        .identity_reducer()
+        .builtin_curry_reducer()
+        .lambda_reducer()
+        .inline_reducer()
+        .remove_no_inlines()
+}
