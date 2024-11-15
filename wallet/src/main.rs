7bd9c3075d2e7fb1d0268f6fe127482c9589129e
Pedro Sánchez Terraf <pedrost@gmail.com>
2024-11-15 15:46:56 -0300

    Ports and the like in main README, unused dep, docstrings in wallet

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -1,4 +1,24 @@
 //! CLI wallet to demostrate spending and minting transactions.
+//!
+//! ## Basic usage
+//! 
+//! In terminal, run the node in development mode:
+//! 
+//! ```bash
+//! ./target/release/griffin-solochain-node --dev
+//! ```
+//! 
+//! In another terminal, one can interact with the node by issuing wallet
+//! commands. Every time the wallet starts (without the `--help` or `--version`
+//! command-line options), it will try to synchronize its database with the
+//! present chain state through RPC port 9944 (the [DEFAULT_ENDPOINT]), unless
+//! there is a mismatch with the genesis hash.
+//! 
+//! To list the whole UTxO set, run
+//! 
+//! ```bash
+//! ./target/release/griffin-wallet show-all-outputs
+//! ```
 
 extern crate alloc;
 
